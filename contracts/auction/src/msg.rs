7bc3db71d893f9ea371ec21a2e64bf2fe2bde0fe
Ethan Plant <plant.ethan@gmail.com>
2022-03-26 04:06:19 -0400

    Contract messages

--- contracts/auction/src/msg.rs
+++ contracts/auction/src/msg.rs
@@ -1,27 +1,36 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+// Initial auction details
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub owner: Addr,
+    pub item_name: String,
+    pub item_desc: String,
+    pub starting_price: f64,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Bid { bid: f64 }, // Place a bid
+    Cancel {}, // Cancel the auction
+    EndAuction {}, // End the auction and pay out the owner
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    // GetAuction returns the current auction details in JSON
+    GetAuction {},
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct CountResponse {
-    pub count: i32,
+    pub item_name: String,
+    pub item_desc: String,
+    pub starting_price: f64,
+    pub highest_bid: f64,
+    pub is_active: bool,
 }
