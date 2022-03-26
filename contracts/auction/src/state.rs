085a837f6868bf667da7530db73c46ecac7f7824
Ethan Plant <plant.ethan@gmail.com>
2022-03-26 03:03:29 -0400

    Auction state

--- contracts/auction/src/state.rs
+++ contracts/auction/src/state.rs
@@ -6,8 +6,13 @@ use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
     pub owner: Addr,
+    pub item_name: String,
+    pub item_desc: String,
+    pub starting_price: f64,
+    pub highest_bid: f64,
+    pub highest_bidder: Addr,
+    pub is_active: bool,
 }
 
 pub const STATE: Item<State> = Item::new("state");
